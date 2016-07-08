0e99f4af65a1dc435dcdac5ff855d6ab0cde786c
Wojtek Franke <enajski@gmail.com>
2016-07-08 16:06:23 +0200

    Enable use of multi-word index

--- src/leviathan_synth/core.clj
+++ src/leviathan_synth/core.clj
@@ -1,21 +1,42 @@
 (ns leviathan-synth.core
-  (:require [clojure.java.io :as io]
-            [clojure.java.shell :refer [sh]]))
+  (:require [leviathan-synth.index :as media-library]
+            [clojure.java.shell :refer [sh]]
+            [clojure.spec :as s]
+            [clojure.string :refer [split join trim replace-first]]))
 
-(defn build-index [path]
-  (->> (io/file path)
-       (file-seq)
-       (filter #(not (.isDirectory %)))
-       (filter #(re-seq (re-pattern ".wav") (str %)))
-       (map #(hash-map (first (re-seq (re-pattern "^[\\w\\s]*\\b") (.getName %))) [(str %)]))
-       (apply merge-with concat)))
+(def media-path (atom (or (System/getenv "MEDIAPATH")
+                          "/Users/dev/experiments/leviathan-synth/media")))
+(def word-index (atom (media-library/build-index @media-path)))
 
-(def word-index (atom (build-index "/Users/dev/experiments/leviathan-synth/media")))
+(defn show-words [] (keys @word-index))
+
+(def with-spaces (re-pattern " "))
+
+(defn find-match [sentence index]
+  (loop [words (split sentence with-spaces)]
+    (if (empty? words)
+      nil
+      (let [current-phrase (join with-spaces words)]
+        (if (or (contains? @index current-phrase) (= 1 (count words)))
+          current-phrase
+          (recur (drop-last words)))))))
+
+(defn split-into-indexed
+  "Splits the given sentence into the longest possible phrases
+   found in the word index"
+  [sentence index]
+  (loop [phrase sentence
+         output []]
+    (if (empty? phrase)
+      output
+      (if-let [match (find-match phrase index)]
+        (recur (trim (replace-first phrase match "")) (conj output match))))))
 
 (defn speak [^String sentence]
-  (let [words (clojure.string/split sentence (re-pattern " "))]
+  (let [words (split-into-indexed sentence word-index)] 
     (map (fn [word]
            (if (contains? @word-index word)
              (sh "afplay" (rand-nth (get @word-index word)))
              (sh "say" word))
            word) words)))
+
---
+++ src/leviathan_synth/index.clj
@@ -0,0 +1,17 @@
+(ns leviathan-synth.index
+  (:require [clojure.java.io :as io]))
+
+(defn build-index-entry [file]
+  (let [k (first (re-seq (re-pattern "^[\\w\\s]*\\b") (.getName file)))
+        v [(str file)]]
+    (hash-map k v)))
+
+(defn build-index [path]
+  (->> (io/file path)
+       (file-seq)
+       (filter #(not (.isDirectory %)))
+       (filter #(re-seq (re-pattern ".wav") (str %)))
+       (map build-index-entry)
+       (apply merge-with concat)))
+
+(keys (build-index "/Users/dev/experiments/leviathan-synth/media"))
