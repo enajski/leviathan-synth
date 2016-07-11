5e1c853df320ffa81836d5cc7bb36282762c9516
Wojtek Franke <enajski@gmail.com>
2016-07-11 13:08:46 +0200

    Refactor index

--- src/leviathan_synth/index.clj
+++ src/leviathan_synth/index.clj
@@ -1,16 +1,43 @@
 (ns leviathan-synth.index
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.spec :as s]))
 
-(defn build-index-entry [file]
-  (let [k (first (re-seq (re-pattern "^[\\w\\s]*\\b") (.getName file)))
+(defn get-phrase-from-filename
+  "Extracts the words preceding the version
+   and file extension"
+  [filename]
+  (->> filename
+       (re-seq (re-pattern "^[\\w\\s]*\\b"))
+       first))
+
+(s/def ::index (s/map-of string? vector?))
+
+(defn build-index-entry
+  [file]
+  (let [filename (.getName file)
+        k (get-phrase-from-filename filename)
         v [(str file)]]
     (hash-map k v)))
 
-(defn build-index [path]
-  (->> (io/file path)
-       (file-seq)
+(defn validate!
+  [index]
+  (if (s/valid? ::index index)
+    index
+    (throw (ex-info "Invalid input" (s/explain-data ::index index)))))
+
+(defn get-files
+  [path]
+  (file-seq (io/file path)))
+
+(defn wav?
+  [file]
+  (re-seq (re-pattern ".wav") (str file)))
+
+(defn build-index
+  [path]
+  (->> (get-files path)
        (filter #(not (.isDirectory %)))
-       (filter #(re-seq (re-pattern ".wav") (str %)))
+       (filter wav?) 
        (map build-index-entry)
        (apply merge-with concat)))
 
