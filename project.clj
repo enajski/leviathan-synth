c52691cc12b565c42c993a634de1810023698384
Wojtek Franke <enajski@gmail.com>
2016-07-13 00:35:27 +0200

    Require dynne for audio

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha7"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha7"]
+                 [org.craigandera/dynne "0.4.1"]])
