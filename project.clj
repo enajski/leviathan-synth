4fee38ae4012e2cac97eeaa9a625aec7348f988f
Wojtek Franke <enajski@gmail.com>
2016-08-01 22:44:30 +0200

    Add cljs deps

--- project.clj
+++ project.clj
@@ -4,4 +4,22 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha7"]
-                 [org.craigandera/dynne "0.4.1"]])
+                 [org.craigandera/dynne "0.4.1"]
+                 [org.clojure/clojurescript "1.9.93"]
+                 [http-kit "2.2.0"]
+                 [compojure "1.5.1"]
+                 [com.stuartsierra/component "0.3.1"]
+                 [domina "1.0.3"]]
+  :main leviathan-synth.system
+  :profiles {:dev {:plugins [[lein-cljsbuild "1.1.3"]
+                             [lein-figwheel "0.5.4-7"]]
+                   :dependencies [[reloaded.repl "0.2.2"]]
+                   :source-paths ["dev"]
+                   :cljsbuild {:builds [{:source-paths ["src" "dev"]
+                                         :figwheel true
+                                         :compiler {:main "leviathan-synth/client"
+                                                    :output-to "target/classes/public/app.js"
+                                                    ;; :output-dir "target/classes/public/out"
+                                                    :optimizations :none
+                                                    :recompile-dependents true
+                                                    :source-map true}}]}}})
