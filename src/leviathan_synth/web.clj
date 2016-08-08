cb1d95c9908e46e718a43afa9df2ec1a947c4f3e
Wojtek Franke <enajski@gmail.com>
2016-08-08 20:18:20 +0200

    Update routes to render and expose audio

--- src/leviathan_synth/web.clj
+++ src/leviathan_synth/web.clj
@@ -1,13 +1,19 @@
 (ns leviathan-synth.web
-  (:require [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [files resources]]))
+  (:require [compojure.core :refer [defroutes GET POST]]
+            [compojure.route :refer [files resources]]
+            [leviathan-synth.core :refer [md5 save-sentence-to-wav]]
+            [clojure.data.json :as json]))
 
-(defn index [req]
-  {:status  200
-   :headers {"Content-Type" "text/html"}
-   :body    "Hello from Compojure!"})
+(defn text-to-wave [text]
+  (println (str "Rendering text: " text))
+  (let [hash (md5 text)
+        result (save-sentence-to-wav text (str "resources/public/" hash ".wav"))]
+    (println result)
+    (str hash ".wav")))
 
 (defroutes app
-  (GET "/" [] index)
+  (POST "/render" {params :body}
+    (let [body (json/read-str (slurp params))]
+      (text-to-wave (body "text"))))
   (resources "/")
   (files "/target" {:root "target"}))
