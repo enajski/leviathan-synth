886eff7f801fe07cad386ccba61cc1b6ee76bc61
Wojtek Franke <enajski@gmail.com>
2016-08-08 20:18:55 +0200

    Add simple playback of sentences to client

--- src/leviathan_synth/client.cljs
+++ src/leviathan_synth/client.cljs
@@ -1,9 +1,18 @@
 (ns leviathan-synth.client
-  (:require [domina :refer [by-id]]
-            [domina.events :refer [listen!]]))
+  (:require [domina :refer [by-id append!]]
+            [domina.events :refer [listen!]]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (defn log [mess]
   (.log js/console mess))
 
-(listen! (by-id "words") :click (log "stało się"))
-;; (goog.events/listen (dom/getWindow) (.-KEY events/EventType) #(js/alert "asda"))
+(defn get-sentence []
+  (.-value (by-id "words")))
+
+(defn insert-rendered-audio [endpoint payload]
+  (go (let [response (<! (http/post endpoint {:json-params payload}))]
+        (append! (by-id "results") (str "<audio src='/" (:body response) "' autoplay></audio>")))))
+
+(listen! (by-id "send") :click (fn [_] (insert-rendered-audio "/render" {:text (get-sentence)})))
