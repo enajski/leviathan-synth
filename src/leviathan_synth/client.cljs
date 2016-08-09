c0d27a5d70122383f444c05766ea8b1f7547614a
Wojtek Franke <enajski@gmail.com>
2016-08-10 00:14:10 +0200

    Add keyboard triggers to play audio

--- src/leviathan_synth/client.cljs
+++ src/leviathan_synth/client.cljs
@@ -1,5 +1,6 @@
 (ns leviathan-synth.client
   (:require [domina :refer [by-id append!]]
+            [domina.css :refer [sel]]
             [domina.events :refer [listen!]]
             [cljs-http.client :as http]
             [cljs.core.async :refer [<!]])
@@ -11,10 +12,17 @@
 (defn get-sentence []
   (.-value (by-id "words")))
 
+(def render-index (atom 0))
+
 (defn insert-rendered-audio [endpoint payload]
   (go (let [response (<! (http/post endpoint {:json-params payload}))
             html (str "<p>" (:text payload) "</p>"
-                      "<audio src='/" (:body response) "'  autoplay controls='controls'></audio>")]
+                      "<audio id='audio" (inc @render-index) "' "
+                        "src='/" (:body response) "' "
+                        "autoplay controls='controls'>"
+                      "</audio>"
+                      "<div class='key-tip'>Press " (inc @render-index) " to trigger</div>")]
+        (swap! render-index inc)
         (append! (by-id "results") html))))
 
 (defn display-available-words []
@@ -28,4 +36,11 @@
 
 (display-available-words)
 
+(def ascii-numbers (range 49 57))
+(defn keycode->number [keycode] (- keycode (dec (first ascii-numbers))))
+
 (listen! (by-id "send") :click (fn [_] (insert-rendered-audio "/render" {:text (get-sentence)})))
+(listen! (sel "body") :keypress (fn [e]
+                                  (let [keycode (:keyCode e)]
+                                    (when (some #{keycode} ascii-numbers)
+                                      (.play (by-id (str "audio" (keycode->number keycode))))))))
